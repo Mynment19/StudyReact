90defa3d77c3ab797390fba6bab2731473d82109
parkjeonghyeon <mynment19@gmail.com>
2024-04-03 20:34:03 +0900

    #8-3,8-4

--- section08/src/components/Editor.jsx
+++ section08/src/components/Editor.jsx
@@ -1,10 +1,34 @@
+import { useState, useRef } from "react";
 import "./Editor.css"
 
-const Editor = () => {
+const Editor = ({ onCreate }) => {
+
+    const [content, setContent] = useState("");
+    const contentRef = useRef();
+
+    const onChangeContent = (e) => {
+        setContent(e.target.value);
+    }
+
+    const onKeyDown = (e) => {
+        if (e.keyCode === 13) {
+            onsubmit();
+        }
+    }
+
+    const onsubmit = () => {
+        if (content === "") {
+            contentRef.current.focus();
+            return;
+        }
+        onCreate(content);
+        setContent("");
+    }
+
     return (
         <div className="Editor">
-            <input placeholder="새로운 Todo..." />
-            <button>추가</button>
+            <input ref={contentRef} value={content} onKeyDown={onKeyDown} onChange={onChangeContent} placeholder="새로운 Todo..." />
+            <button onClick={onsubmit}>추가</button>
         </div>
     );
 };
