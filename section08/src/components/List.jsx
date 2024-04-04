f51728cb2cf07e81ddfb2a30bd72b28d57c3d8ba
parkjeonghyeon <mynment19@gmail.com>
2024-04-04 20:47:39 +0900

    #8-6. 8-7

--- section08/src/components/List.jsx
+++ section08/src/components/List.jsx
@@ -2,7 +2,7 @@ import "./List.css"
 import TodoItem from "./TodoItem";
 import {useState} from "react";
 
-const List = ({todos}) => {
+const List = ({todos, onUpdate, onDelete}) => {
     const [search, setSearch] = useState("");
 
     const onChangeSearch = (e) => {
@@ -13,9 +13,9 @@ const List = ({todos}) => {
         if (search === ''){
             return todos;
         }
-        return todos.filter((todo) => {
-            todo.content.includes(search);
-        })
+        return todos.filter((todo) => 
+            todo.content.toLowerCase().includes(search.toLowerCase())
+        );
     }
 
     const filteredTodos = getFilteredData();
@@ -26,7 +26,7 @@ const List = ({todos}) => {
             <input value={search} onChange={onChangeSearch} placeholder="검색어를 입력하세요" />
             <div className="todos_wrapper">
                 {filteredTodos.map((todo)=> {
-                    return <TodoItem key={todo.id} {...todo}/>;
+                    return <TodoItem key={todo.id} {...todo} onUpdate={onUpdate} onDelete={onDelete}/>;
                 })}
             </div>
         </div>
