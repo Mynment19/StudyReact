57bd8de465ce197f166920bb1b75feae92528210
PJH <mynmnet19@entropykorea.com>
2024-04-09 17:04:32 +0900

    #10-3, 10-4

--- section10/src/App.jsx
+++ section10/src/App.jsx
@@ -1,28 +1,28 @@
-import {useRef, useReducer} from 'react';
+import {useRef, useReducer, useCallback} from 'react';
 import './App.css'
 import Editor from './components/Editor';
 import Header from './components/Header';
 import List from './components/List';
 
 const mockData = [
-  {
-    id: 0,
-    isDone: false,
-    content: "React 공부하기",
-    date: new Date().getTime(),
-  },
-  {
-    id: 1,
-    isDone: false,
-    content: "빨래하기",
-    date: new Date().getTime(),
-  },
-  {
-    id: 2,
-    isDone: false,
-    content: "노래 연습하기",
-    date: new Date().getTime(),
-  }
+    {
+        id: 0,
+        isDone: false,
+        content: "React 공부하기",
+        date: new Date().getTime(),
+    },
+    {
+        id: 1,
+        isDone: false,
+        content: "빨래하기",
+        date: new Date().getTime(),
+    },
+    {
+        id: 2,
+        isDone: false,
+        content: "노래 연습하기",
+        date: new Date().getTime(),
+    }
 ];
 
 function reducer(state, action) {
@@ -42,7 +42,7 @@ function App() {
     const [todos, dispatch] = useReducer(reducer, mockData);
     const idRef = useRef(3);
 
-    const onCreate = (content) => {
+    const onCreate = useCallback((content) => {
         dispatch({
             type: 'CREATE',
             data: {
@@ -52,21 +52,21 @@ function App() {
                 date: new Date().getTime(),
             }
         })
-    }
+    }, []);
 
-    const onUpdate = (targetId) => {
+    const onUpdate = useCallback((targetId) => {
         dispatch({
             type: 'UPDATE',
             targetId: targetId,
         })
-    }
+    }, []);
 
-    const onDelete = (targetId) => {
+    const onDelete = useCallback((targetId) => {
         dispatch({
             type: "DELETE",
             targetId: targetId,
         })
-    }
+    }, []);
 
     return (
         <div className='App'>
