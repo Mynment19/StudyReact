a6580f3999bf34ec0c82c51ea0c12cd21aa91b67
parkjeonghyeon <mynment19@gmail.com>
2024-04-18 21:42:15 +0900

    #12-8

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,54 +1,41 @@
 import './App.css'
-import { Routes, Route, Link, useNavigate } from "react-router-dom";
-import Home from "../src/pages/Home.jsx";
-import New from "../src/pages/New.jsx";
-import Diary from "../src/pages/Diary.jsx";
-import Notfound from "../src/pages/Notfound.jsx";
-import { getEmotionImage } from './util/get-emotion-img.js';
-import Button from './components/Button.jsx';
-import Header from './components/Header.jsx';
+import { useReducer } from 'react';
+import { Routes, Route} from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import New from "./pages/New.jsx";
+import Diary from "./pages/Diary.jsx";
+import Edit from './pages/Edit.jsx';
+import Notfound from "./pages/Notfound.jsx";
 
-// 1. "/" : 모든 일기를 조회하는 Home 페이지
-// 2. "/new" : 새로운 일기를 작성하는 New 페이지
-// 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
+const mockData = [
+    {   
+        id:1,
+        createdDate : new Date().getTime(),
+        emotionId : 1,
+        contetn : "1번 일기 내용",
+    },
+    {   
+        id : 2,
+        createdDate : new Date().getTime(),
+        emotionId : 2,
+        contetn : "2번 일기 내용",
+    },
+]
+
+function reducer(state, action) {
+    return state;
+}
+
 function App() {
-    const nav = useNavigate();
-
-    const onClickButton = () => {
-        nav("/new")
-    }
+    const [data, dispatch] = useReducer(reducer, mockData);
 
     return (
         <>
-
-            <Header
-                title={"Header"}
-                leftChild={<Button text={"Left"} />}
-                rightChild={<Button text={"Right"} />}
-            />
-            <Button
-                text={"123"}
-                onClick={() => {
-                    console.log("123번 버튼 클릭")
-                }} />
-
-            <Button
-                text={"123"}
-                type={"POSITIVE"}
-                onClick={() => {
-                    console.log("123번 버튼 클릭")
-                }} />
-
-            <Button
-                text={"123"}
-                type={"NEGATIVE"}
-                onClick={() => {
-                    console.log("123번 버튼 클릭")
-                }} />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/new" element={<New />} />
                 <Route path="/diary/:id" element={<Diary />} />
+                <Route path="/edit/:id" element={<Edit />} />
                 <Route path="*" element={<Notfound />} />
             </Routes>
         </>
