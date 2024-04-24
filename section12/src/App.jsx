361e11d97860a400f19c9b3a0d526ad86da8ca48
JeongHyeon Park <mynment19@gmail.com>
2024-04-24 23:42:52 +0900

    #12-15

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,36 +1,30 @@
 import './App.css'
-import { useReducer, useRef, createContext } from 'react';
-import { Routes, Route } from "react-router-dom";
+import { createContext, useReducer, useRef } from "react";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.jsx";
-import New from "./pages/New.jsx";
-import Diary from "./pages/Diary.jsx";
-import Edit from './pages/Edit.jsx';
-import Notfound from "./pages/Notfound.jsx";
+import New from "./pages/New";
+import Diary from "./pages/Diary";
+import Edit from "./pages/Edit.jsx";
+import Notfound from "./pages/Notfound";
 
 const mockData = [
     {
         id: 1,
         createdDate: new Date("2024-04-19").getTime(),
         emotionId: 1,
-        content: "1번 일기 내용",
+        content: "1번 일기 내용"
     },
     {
         id: 2,
         createdDate: new Date("2024-04-18").getTime(),
         emotionId: 2,
-        content: "2번 일기 내용",
+        content: "2번 일기 내용"
     },
     {
         id: 3,
         createdDate: new Date("2024-03-14").getTime(),
         emotionId: 3,
-        content: "성민이 생일",
-    },
-    {
-        id: 4,
-        createdDate: new Date("2023-10-31").getTime(),
-        emotionId: 4,
-        content: "가장 좋아하는 날",
+        content: "3번 일기 내용"
     },
 ]
 
@@ -39,23 +33,20 @@ function reducer(state, action) {
         case "CREATE":
             return [action.data, ...state];
         case "UPDATE":
-            return state.map((item) =>
-                String(item.id) === String(action.data.id) ? action.id : item
-            );
+            return state.map(item => String(item.id) === String(action.data.id) ? action.data : item);
         case "DELETE":
-            return state.filter((item) => String(item.id) !== String(action.id));
+            return state.filter(item => String(item.id) !== String(action.data.id));
         default:
             return state;
     }
 }
 
-export const DiaryStateContext = createContext();
-export const DiaryDispatchContext = createContext();
+export const DiaryStateContext = createContext()
+export const DiaryDispatchContext = createContext()
 
 function App() {
     const [data, dispatch] = useReducer(reducer, mockData);
     const idRef = useRef(3);
-
     // 새로운 일기 추가
     const onCreate = (createdDate, emotionId, content) => {
         dispatch({
@@ -88,16 +79,14 @@ function App() {
             type: "DELETE",
             data: {
                 id,
-            }
+            },
         })
     }
 
     return (
         <>
             <DiaryStateContext.Provider value={data}>
-                <DiaryDispatchContext.Provider value={{
-                    onCreate, onUpdate, onDelete,
-                }}>
+                <DiaryDispatchContext.Provider value={{ onCreate, onUpdate, onDelete }}>
                     <Routes>
                         <Route path="/" element={<Home />} />
                         <Route path="/new" element={<New />} />
@@ -108,7 +97,7 @@ function App() {
                 </DiaryDispatchContext.Provider>
             </DiaryStateContext.Provider>
         </>
-    );
+    )
 }
 
 export default App
--- section12/src/components/Editor.jsx
+++ section12/src/components/Editor.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Button from "./Button";
 import "./Editor.css";
 import EmotionItem from "./EmotionItem";
@@ -43,13 +43,22 @@ const getStringedDate = (targetDate) => {
     return `${year}-${month}-${date}`
 }
 
-const Editor = ({ onSubmit }) => {
+const Editor = ({ initData, onSubmit }) => {
     const [input, setInput] = useState({
         createdDate: new Date(),
         emotionId: 3,
         content: "",
     });
     const nav = useNavigate();
+
+    useEffect(() => {
+        if (initData) {
+            setInput({
+                ...initData,
+                createdDate: new Date(Number(initData.createdDate)),
+            });
+        };
+    }, [initData]);
 
     const onChangeInput = (e) => {
         let name = e.target.name;
